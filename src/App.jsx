e3710858b77be85c23cbf5ec76eeb37e03ff00f6
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-04-24 13:41:30 +0530

    1st commit on react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+<Chai/>
+<p>it's under fragment.</p>
+</>
+  )
+}
+
+export default App
---
+++ src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return (
+        <h3>hii, my name is shubha! and i like chai.</h3>
+    )
+}
+export default Chai
